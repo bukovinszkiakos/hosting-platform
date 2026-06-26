d36563148ea69560cdde00a6bc06529de250f2ad
Akos Bukovinszki <nashyro1995@gmail.com>
2026-06-26 14:20:50 +0200

    Complete Task 9 - configure Program.cs

--- backend/src/HostingPlatform.Api/Program.cs
+++ backend/src/HostingPlatform.Api/Program.cs
@@ -1,21 +1,36 @@
+using HostingPlatform.Api.Configuration;
+using HostingPlatform.Api.Data;
+using HostingPlatform.Api.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.Configure<AwsSettings>(builder.Configuration.GetSection("AWS"));
+builder.Services.Configure<AuthenticationSettings>(builder.Configuration.GetSection("Authentication"));
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<User, IdentityRole<Guid>>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAuthorization();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
